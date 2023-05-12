155a73e1334f0ddf8ffc057608fd5630ef888da0
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-12 12:01:06 +0530

    Add files via upload

--- Creating Login Screen Frontend/app.js
+++ Creating Login Screen Frontend/app.js
@@ -1,1 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const sequelize = require("./util/database");
+const User = require("./models/userModel");
 
+const app = express();
+
+const userRoute = require("./route/user");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/user", userRoute);
+app.use("/user", userRoute);
+
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
