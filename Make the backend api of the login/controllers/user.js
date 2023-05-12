f7130c62441f7f6c8ed1b1071601a76a81230a88
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-12 17:21:50 +0530

    Add files via upload

--- Make the backend api of the login/controllers/user.js
+++ Make the backend api of the login/controllers/user.js
@@ -1,1 +1,45 @@
+const { User } = require("../models/userModel");
 
+exports.signUp = async (req, res, next) => {
+  const UserName = req.body.name;
+  const Email = req.body.email;
+  const PassWord = req.body.password;
+  const user = await User.findOne({ where: { email: Email } });
+  try {
+    if (user) {
+      return res.status(201).json({ err: "email already exits" });
+    } else {
+      User.create({
+        username: UserName,
+        email: Email,
+        password: PassWord,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const email = req.body.Email;
+    const password = req.body.Password;
+
+    const user = await User.findOne({
+      where: { email: email },
+    });
+
+    if (user) {
+      if (user.password != password) {
+        return res.status(401).json({ msg: "User not authorized" });
+        // return res.status(401);
+      } else {
+        return res.status(201).json({ msg: "login successfully" });
+      }
+    } else {
+      return res.status(404).json({ msg: "User does not exits" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
