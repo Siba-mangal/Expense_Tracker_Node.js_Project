47ac8a832470eca5228e6fe2c9132e9d97366bdb
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-12 21:23:31 +0530

    Add files via upload

--- Your user's password is at risk!!!!/route/user.js
+++ Your user's password is at risk!!!!/route/user.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const path = require("path");
 
+const userController = require("../controllers/user");
+
+const router = express.Router();
+
+router.post("/signup", userController.signUp);
+router.post("/login", userController.login);
+
+module.exports = router;
