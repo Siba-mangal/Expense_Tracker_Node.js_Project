058c8ed9b27344cff9e43f2526f48a434b254dc8
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-14 13:27:32 +0530

    Add files via upload

--- Daily Expense App features/route/user.js
+++ Daily Expense App features/route/user.js
@@ -1,1 +1,14 @@
+const express = require("express");
+const path = require("path");
 
+const userController = require("../controllers/user");
+const expenseController = require("../controllers/expense");
+
+const router = express.Router();
+
+router.post("/signup", userController.signUp);
+router.post("/login", userController.login);
+router.post("/add-expense", expenseController.addExpense);
+router.get("/get-expense", expenseController.getExpense);
+router.delete("/delete-expense/:UserId", expenseController.deleteExpense);
+module.exports = router;
