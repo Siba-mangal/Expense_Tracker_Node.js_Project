46f0bf4d6b46cdd1b4e716ae70a4305baa6795ac
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-15 09:41:04 +0530

    Add files via upload

--- Daily expense app with user Authentication/app.js
+++ Daily expense app with user Authentication/app.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const sequelize = require("./util/database");
 
+const app = express();
+
+const userRoute = require("./route/user");
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/user", userRoute);
+
+sequelize
+  .sync()
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
