5b4c6b79095207f25b316ee5425c366e88828440
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-05-16 13:01:26 +0530

    Add files via upload

---
+++ Daily expense app with user Authentication/controllers/expense.js
@@ -0,0 +1,54 @@
+const expense = require("../models/expense");
+
+exports.addExpense = function (req, res, next) {
+  try {
+    if (!req.body.category) {
+      console.error("Please enter a category");
+    }
+
+    const price = req.body.price;
+    const product = req.body.productName;
+    const category = req.body.category;
+    console.log(req.user.id);
+
+    expense
+      .create({
+        price: price,
+        productName: product,
+        category: category,
+        signupId: req.user.id,
+      })
+      .then((expense) => {
+        return res.status(201).json({ expense });
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.getExpense = async (req, res, next) => {
+  await expense
+    .findAll({ where: { signupId: req.user.id } })
+    .then((expense) => {
+      return res.status(201).json({ expense });
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.deleteExpense = async (req, res, next) => {
+  const expense_id = req.params.UserId;
+  const cato = await expense.findOne({ where: { id: expense_id } });
+
+  expense
+    .destroy({ where: { id: expense_id, signupId: req.user.id } })
+    .then(() => {
+      return res.status(201).json({ category: cato.category, id: cato.id });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(403).json({ success: true, message: "Failed" });
+    });
+};
--- Daily expense app with user Authentication/controllers/user.js
+++ Daily expense app with user Authentication/controllers/user.js
@@ -1,1 +1,56 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+//using bcrypt for password form of hashing
+exports.signUp = async function (req, res, next) {
+  try {
+    const UserName = req.body.name;
+    const Email = req.body.email;
+    const PassWord = req.body.password;
+    const user = await User.findOne({ where: { email: Email } });
+    if (user) {
+      return res.status(201).json({ err: "email already exits" });
+    } else {
+      const salt = await bcrypt.genSalt(10);
+      const hashPassword = await bcrypt.hash(PassWord, salt);
+      console.log(hashPassword);
+      User.create({
+        username: UserName,
+        email: Email,
+        password: hashPassword,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
+const generateAccessToken = (id, name) => {
+  return jwt.sign({ signupId: id, username: name }, "secretkey");
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const email = req.body.Email;
+    const password = req.body.Password;
+    const user = await User.findOne({
+      where: { email: email },
+    });
+    if (user) {
+      const isMatch = bcrypt.compare(password, user.password);
+      if (isMatch) {
+        // console.log(user.id);
+        return res.status(201).json({
+          msg: "login successfully",
+          token: generateAccessToken(user.id, user.username),
+        });
+      } else {
+        return res.status(401).json({ msg: "User not authorized" });
+      }
+    } else {
+      return res.status(404).json({ msg: "User does not exits" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
