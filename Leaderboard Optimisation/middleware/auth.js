257919f0508ea4b1326ce2dfca2eca3617b457d3
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-06-02 18:22:11 +0530

    Add files via upload

--- Leaderboard Optimisation/middleware/auth.js
+++ Leaderboard Optimisation/middleware/auth.js
@@ -1,1 +1,23 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
+const authenticate = (req, res, next) => {
+  try {
+    const token = req.header("Authorization");
+    console.log(token);
+    const user = jwt.verify(token, "secretkey");
+    console.log("userID >>>> ", user.signupId);
+    User.findByPk(user.signupId).then((user) => {
+      req.user = user; ///ver
+      next();
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ success: false });
+    // err
+  }
+};
+
+module.exports = {
+  authenticate,
+};
