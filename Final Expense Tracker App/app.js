48d06ecc07ff10ab4c0f3cb022e925373ddcc198
siba.mangal <63948501+Siba-mangal@users.noreply.github.com>
2023-06-06 13:00:09 +0530

    Add files via upload

--- Final Expense Tracker App/app.js
+++ Final Expense Tracker App/app.js
@@ -6,11 +6,13 @@ const sequelize = require("./util/database");
 const Users = require("./models/userModel");
 const Expenses = require("./models/expense");
 const Order = require("./models/Order");
+const reset = require("./models/forgotPassword");
 
 const userRoute = require("./route/user");
 const expenseRoute = require("./route/expense");
 const purchaseRoutes = require("./route/purchase");
 const premiumRoute = require("./route/premium");
+const forgotPassword = require("./route/forgotPassword");
 
 const app = express();
 const dotenv = require("dotenv");
@@ -25,12 +27,16 @@ app.use("/user", userRoute);
 app.use("/expense", expenseRoute);
 app.use("/purchase", purchaseRoutes);
 app.use("/premium", premiumRoute);
+app.use("/password", forgotPassword);
 
 Users.hasMany(Expenses);
 Expenses.belongsTo(Users);
 
 Users.hasMany(Order);
 Order.belongsTo(Users);
+
+Users.hasMany(reset);
+reset.belongsTo(Users);
 
 sequelize
   .sync()
